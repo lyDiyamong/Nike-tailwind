e221c4adff8e9d2af3eab4b234f0cfa7703984d7
AnB School 0033 <anbschool0033@AnBs-Mac-mini-65.local>
2024-04-11 17:51:24 +0700

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      backgroundColor : {
+        mong : '#573131'
+      },
+      fontFamily : {
+        'roboto' : ['Roboto', 'san-serif']
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
